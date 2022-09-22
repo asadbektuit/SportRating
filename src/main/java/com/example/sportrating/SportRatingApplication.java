108ef5383d1118cd9b39960073103266b4d89222
asadbektuit <davlatovasadbek1@gmail.com>
2022-09-23 01:57:58 +0500

    Initial commit

---
+++ src/main/java/com/example/sportrating/SportRatingApplication.java
@@ -0,0 +1,13 @@
+package com.example.sportrating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportRatingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sportrating/SportRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sportrating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportRatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
